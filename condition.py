9c271eb68b81cee861b408c08d48533a0f5725c9
jack yao <yaowenqiang111@163.com>
2014-05-21 00:43:52 +0800

    variablize the len(sys.arg) value

--- condition.py
+++ condition.py
@@ -1,15 +1,17 @@
 # sys is required to access 'argv'
 import sys
 # Perform check of number of supplied position parameters
-if len(sys.argv) <2:
-    print ("Need AT LEST 1 ARG,Supplied:",len(sys.argv),"ARGs",sys.argv[0])
+#assign length of sys.argv to simple var
+leng1 = len(sys.argv)
+if leng1 <2:
+    print ("Need AT LEST 1 ARG,Supplied:",leng1,"ARGs",sys.argv[0])
     exit(200)
     #exit('Insufficient ARGs')
-elif len(sys.argv) > 2:
-    print("You have supplied too many ARGs",len(sys.argv))
+elif leng1 > 2:
+    print("You have supplied too many ARGs",leng1)
     exit('TOO MANY ARGs')
 else:
-    print ("You have Supplied ",len(sys.argv),"ARGs")
+    print ("You have Supplied ",leng1,"ARGs")
     #pass keeps the functions/script going without doing any thing
     pass
     #help('pass')
