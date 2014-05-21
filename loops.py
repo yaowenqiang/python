1343911bc551617cf33c297bee0a588486685f50
jack yao <yaowenqiang111@163.com>
2014-05-21 23:52:52 +0800

    dd the loop example file

---
+++ loops.py
@@ -0,0 +1,22 @@
+fname = 'jack'
+#for i in fname:
+    #print(i)
+lastname = 'yao'
+full_name = [fname,lastname]
+for i in full_name:
+    print(i)
+for j in range(10):
+    print(j)
+for j in range(1,11):
+    print(j)
+for i,j in enumerate(fname):
+    print(i,j)
+for i in fname:
+    print(i)
+else:
+    print 'loop finished'
+world_leaders = {'UK':'David Cameron','Germany':'Angela Merkl'}
+for i,j in world_leaders.items():
+    print(i,j)
+else:
+    print('finished')
