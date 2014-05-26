20b9128f2f2f30d31b714ae687f43690e1231a55
jack yao <yaowenqiang111@163.com>
2014-05-27 00:14:26 +0800

    add the bits and architecture and uname check code

--- platform_check.py
+++ platform_check.py
@@ -1,4 +1,30 @@
-import platform,re
+import platform,re,sys
 #Obtain system hostname
 h = platform.node()
-print(h)
+#print(h)
+
+#ensure that platform is Windows
+s = platform.system()
+p = re.compile(r'win.',re.IGNORECASE)
+if p.search(s):
+    print("platform is ",s)
+else:
+    print("This script is restricted to Windows platform")
+
+#Ensure 64-bit platform
+bits = platform.architecture()[0][0:2]
+#print(bits)
+p = re.compile(r'^64')
+if p.search(bits):
+    print("BITS; ",bits)
+else:
+    print("SCRIPT:",sys.argv[0]," REQUIRES 64-bit platform")
+#Ensure Release of Windows >= 7
+r = int(platform.release()[0])
+minr = int(7)
+if r >= minr:
+    print("Platform Version:",s,r,bits)
+uname = platform.uname()
+d1 = ''
+u = uname.join(uname)
+print(u)
