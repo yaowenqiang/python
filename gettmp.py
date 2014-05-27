9990823f4b06cb393a669100967c782ea1fa1b06
jack yao <yaowenqiang111@163.com>
2014-05-27 23:45:47 +0800

    add the subprocess demo code

---
+++ gettmp.py
@@ -0,0 +1,20 @@
+from subprocess import *
+import re
+try:
+    #Execute Process
+    #output = Popen(['pcsensor',"-c"],stdout=PIPE).communicate()[0]
+    output = Popen(['uptime'],stdout=PIPE).communicate()[0]
+    print(output)
+    #Parse results with RegEx
+    p = re.compile(r'\s+')
+    s = p.split(output)
+    print(s)
+    s.pop()
+    #assign interesting Elements to new VARs
+    d = s[0]
+    t = s[1]
+    tmp = s[3]
+    print("DATE: ",d," TIME: ",t," TEMPERATURE: ",tmp)
+except OSError as err1:
+    print(err1)
+
