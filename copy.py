dd163d25681b08bce6e0d55044ac4158dfd0c91e
yaowenqiang <yaowenqiang111@163.com>
2014-06-13 01:23:31 +0800

    add open browser functionality

--- copy.py
+++ copy.py
@@ -1,5 +1,13 @@
-import pyperclip,sys,re
-content =str(pyperclip.paste())
+import pyperclip,sys,re,webbrowser
+filename = 'clipboard.txt'
+f1 = open(filename,'r')
+
+content = f1.read()
+if content == '':
+	f2 = open(filename,'w')
+	content =str(pyperclip.paste())
+	f2.write(content)
+
 n = int(sys.argv[1])-1
 p = re.compile(r'\d+')
 i = p.findall(content)
@@ -7,3 +15,4 @@ print(i)
 r = i[n]
 paste = "http://huodong.duomi.com/songtaste/?songid="+r
 pyperclip.copy(paste)
+webbrowser.open("baidu.com")
